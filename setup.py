247792986ad2cfab6ede59d4c4f80db63b855424
Denzil Charleson <as17simpson@gmail.com>
2022-09-03 21:16:29 +0100

    making a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name = 'CNN_conversion',
+  version = '0.0.1',
+  description = 'Convert a Dataset into an image and run a CNN on it',
+  long_description = open('README.md').read(),
+  author = 'Andrew Simpson',
+  url = 'https://github.com/as17simpson/CNN_conversion',
+  license = 'unlicense',
+  packages = ['CNN_conversion'],
+)
